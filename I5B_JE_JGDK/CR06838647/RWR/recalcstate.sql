3275070601382fc5cc71ee9778fb62bd7991d3ed
v5x1239 <48722083+v5x1239@users.noreply.github.com>
2019-03-27 12:59:20 -0400

    Add files via upload

---
+++ I5B_JE_JGDK/CR06838647/RWR/recalcstate.sql
@@ -0,0 +1,28 @@
+/*
+**desc: recalculate the state for inform
+** login as trialuid and trialpid
+*/
+//set echo on;
+spool recalcstate.log
+
+UPDATE PF_SUBJECTVECHAPTERPAGE 
+   SET NeedsRecalcState = 1
+ WHERE NeedsRecalcState IS NULL
+   AND startedstate = 1
+   AND pageid IN (SELECT DISTINCT pageid 
+                    FROM PF_PAGE 
+                   WHERE pagerefname in ('IPD3001_MH8001_F1')
+                  )
+   AND subjectchapterid IN (SELECT subjectchapterid 
+                              FROM PF_SUBJECTVECHAPTER 
+                             WHERE subjectkeyid IN (SELECT patientid 
+                                                      FROM PF_PATIENT 
+                                                    WHERE state > 3
+                                                   )
+                            )
+  AND DELETEDDYNAMICFORMSTATE <> 1
+  AND bitand(STATE,8192) <> 8192;
+
+COMMIT;
+spool off;
+
