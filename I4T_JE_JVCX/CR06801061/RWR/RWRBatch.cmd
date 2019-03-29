b885b521d48bf87e146416363eb1c66533df4397
v5x1239 <48722083+v5x1239@users.noreply.github.com>
2019-03-29 07:28:10 -0400

    Add files via upload

---
+++ I4T_JE_JVCX/CR06801061/RWR/RWRBatch.cmd
@@ -0,0 +1,63 @@
+@echo off
+@if [%1]==[] goto usage
+@if [%2]==[] goto usage
+@if [%3]==[] goto usage
+goto start
+:usage
+@echo RWRBatch.cmd db instance name] [UID] [PID]
+@echo This file is used for InForm RWR changes, to generate MedML based on the SQL statements provided.
+@echo Customers should edit this file with the appropriate SQL file names and submit with the implementation ticket..
+goto end
+
+goto start
+
+:start
+@set dbinstance=%1
+@set uid=%2
+@set pid=%3
+
+cd>%TEMP%\local_dir.tmp
+set OUTBOUND=%TEMP%\local_dir.tmp
+
+@echo RWRBatch.cmd running...
+@echo RWRBatch.cmd running... >> RWRBatch.log
+@echo DB instance %1
+@echo DB instance %1 >> RWRBatch.log
+@echo UID %2
+@echo UID %2 >> RWRBatch.log
+
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateFormSection_igEX1001_F2_S1_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateFormSection_igEX1001_F4_S1_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateFormSection_igEXCMP1001_F1_S1_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateFormSection_igEXCMP1001_F2_S1_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateFormSection_igSAE2001_F1_S3_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_EXDOSADJ_CMPD_EX1001_F1_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_EXDOSADJ_CMPD_EX1001_F3_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_EXPDOSEU_EX1001_F1_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateFormSection_igCIV1001_F1_S1_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateFormSection_igSYST3001_F1_S2_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_SYSTRSDAT_SYST3001_F1_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_STAT_SUBBSL_STAT1001_F1_1.sql >> RWRBatch.log
+
+
+
+
+@echo >> RWRBatch.log
+
+if [%ERRORLEVEL%]==[0] @echo XMLs Generated successfully
+if not [%ERRORLEVEL%]==[0] @echo **Error! XML Generation
+if [%ERRORLEVEL%]==[0] @echo XMLs Generated successfully >> RWRBatch.log
+if not [%ERRORLEVEL%]==[0] @echo **Error! XML Generation >> RWRBatch.log
+
+if not [%ERRORLEVEL%]==[0] goto END
+
+RWRXMLs.vbs %OUTBOUND%
+
+if [%ERRORLEVEL%]==[0] @echo RWRXMLs.xml Generated successfully
+if not [%ERRORLEVEL%]==[0] @echo **Error! RWRXMLs.xml Generation
+if [%ERRORLEVEL%]==[0] @echo RWRXMLs.xml Generated successfully >> RWRBatch.log
+if not [%ERRORLEVEL%]==[0] @echo **Error! RWRXMLs.xml Generation >> RWRBatch.log
+
+exit /B %ERRORLEVEL%
+
+:end
---
+++ I4T_JE_JVCX/CR06801061/SQLs/Batch_File.sql
@@ -0,0 +1,4 @@
+@fixdynamics.sql;
+@UndeleteForm_INF_15385.sql;
+@InactivateCCTriggers.sql;
+@recalcstate.sql;
