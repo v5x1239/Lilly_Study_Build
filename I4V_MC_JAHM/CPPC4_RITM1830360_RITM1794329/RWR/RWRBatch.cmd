7589b5638722840cb44caf929f234c506109dd10
v5x1239 <48722083+v5x1239@users.noreply.github.com>
2019-03-29 08:03:32 -0400

    Add files via upload

---
+++ I4V_MC_JAHM/CPPC4_RITM1830360_RITM1794329/RWR/RWRBatch.cmd
@@ -0,0 +1,105 @@
+@echo off
+@if [%1]==[] goto usage
+@if [%2]==[] goto usage
+@if [%3]==[] goto usage
+goto start
+:usage
+@echo RWRBatch.cmd db instance name] [UID] [PID]
+@echo This file is used for InForm RWR changes, to generate MedML based on the SQL statements provided.
+@echo Customers should edit this file with the appropriate SQL file names and submit with the implementation ticket..
+goto end
+
+goto start
+
+:start
+@set dbinstance=%1
+@set uid=%2
+@set pid=%3
+
+cd>%TEMP%\local_dir.tmp
+set OUTBOUND=%TEMP%\local_dir.tmp
+
+@echo RWRBatch.cmd running...
+@echo RWRBatch.cmd running... >> RWRBatch.log
+@echo DB instance %1
+@echo DB instance %1 >> RWRBatch.log
+@echo UID %2
+@echo UID %2 >> RWRBatch.log
+
+
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateFormSection_CM1001_LF1_igCM1001_F1_S2_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateFormSection_CM1001_LF1_igCM1001_F1_S2_2.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateFormSection_CM1001_LF1_igCM1001_F1_S2_3.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateFormSection_CM1001_LF1_igCM1001_F1_S2_4.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateFormSection_CM4001_LF1_igCM4001_LF1_S1_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateFormSection_CM4001_LF1_igCM4001_LF1_S1_2.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateFormSection_CM4001_LF1_igCM4001_LF1_S1_3.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateFormSection_CM4001_LF1_igCM4001_LF1_S1_4.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateFormSection_CM4001_C1LF1_igCM4001_C1LF1_S1_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateFormSection_CM4001_C1LF1_igCM4001_C1LF1_S1_2.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateFormSection_CM4001_C1LF1_igCM4001_C1LF1_S1_3.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateFormSection_CM4001_C1LF1_igCM4001_C1LF1_S1_4.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateFormSection_CM2001_LF2_igCM2001_F2_S2_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateFormSection_CM2001_LF2_igCM2001_F2_S2_2.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateFormSection_CM2001_LF2_igCM2001_F2_S2_3.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateFormSection_CM2001_LF2_igCM2001_F2_S2_4.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateFormSection_CM1001_LF2_igCM1001_F2_S2_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateFormSection_CM1001_LF2_igCM1001_F2_S2_2.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateFormSection_CM1001_LF2_igCM1001_F2_S2_3.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateFormSection_CM1001_LF2_igCM1001_F2_S2_4.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateFormSection_MHPRESP1001_C1LF1_igMHPRESP1001_C1LF1_S2_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateFormSection_MHPRESP1001_C1LF1_igMHPRESP1001_C1LF1_S2_2.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateFormSection_MHPRESP1001_C1LF1_igMHPRESP1001_C1LF1_S2_3.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateFormSection_MHPRESP1001_C1LF1_igMHPRESP1001_C1LF1_S2_4.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igCM4001_F1_S2_CMCLAS_CM4_F1_S2_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igCM4001_F1_S2_CMCLAS_CM4_F1_S2_2.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igCM4001_F1_S2_CMCLAS_CM4_F1_S2_3.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igCM4001_F1_S2_CMCLAS_CM4_F1_S2_4.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igCM4001_F1_S2_CMDECOD_CM4_F1_S2_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igCM4001_F1_S2_CMDECOD_CM4_F1_S2_2.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igCM4001_F1_S2_CMDECOD_CM4_F1_S2_3.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igCM4001_F1_S2_CMDECOD_CM4_F1_S2_4.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igCM3001_F1_S1_CMDECOD_CM3001_F1_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igCM3001_F1_S1_CMDECOD_CM3001_F1_2.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igCM3001_F1_S1_CMDECOD_CM3001_F1_3.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igCM3001_F1_S1_CMDECOD_CM3001_F1_4.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igCM3001_F1_S1_CMCLAS_CM3001_F1_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igCM3001_F1_S1_CMCLAS_CM3001_F1_2.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igCM3001_F1_S1_CMCLAS_CM3001_F1_3.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igCM3001_F1_S1_CMCLAS_CM3001_F1_4.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igCMSI1001_F2_S1_CMDECOD_CMSI1_F2_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igCMSI1001_F2_S1_CMDECOD_CMSI1_F2_2.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igCMSI1001_F2_S1_CMDECOD_CMSI1_F2_3.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igCMSI1001_F2_S1_CMDECOD_CMSI1_F2_4.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igCMSI1001_F2_S1_CMCLAS_CMSI1_F2_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igCMSI1001_F2_S1_CMCLAS_CMSI1_F2_2.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igCMSI1001_F2_S1_CMCLAS_CMSI1_F2_3.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igCMSI1001_F2_S1_CMCLAS_CMSI1_F2_4.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igCM4001_F1_S1_CMDECOD_CM4_F1_S1_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igCM4001_F1_S1_CMDECOD_CM4_F1_S1_2.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igCM4001_F1_S1_CMDECOD_CM4_F1_S1_3.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igCM4001_F1_S1_CMDECOD_CM4_F1_S1_4.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igCM4001_F1_S1_CMCLAS_CM4_F1_S1_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igCM4001_F1_S1_CMCLAS_CM4_F1_S1_2.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igCM4001_F1_S1_CMCLAS_CM4_F1_S1_3.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igCM4001_F1_S1_CMCLAS_CM4_F1_S1_4.sql >> RWRBatch.log
+
+@echo >> RWRBatch.log
+
+if [%ERRORLEVEL%]==[0] @echo XMLs Generated successfully
+if not [%ERRORLEVEL%]==[0] @echo **Error! XML Generation
+if [%ERRORLEVEL%]==[0] @echo XMLs Generated successfully >> RWRBatch.log
+if not [%ERRORLEVEL%]==[0] @echo **Error! XML Generation >> RWRBatch.log
+
+if not [%ERRORLEVEL%]==[0] goto END
+
+RWRXMLs.vbs %OUTBOUND%
+
+if [%ERRORLEVEL%]==[0] @echo RWRXMLs.xml Generated successfully
+if not [%ERRORLEVEL%]==[0] @echo **Error! RWRXMLs.xml Generation
+if [%ERRORLEVEL%]==[0] @echo RWRXMLs.xml Generated successfully >> RWRBatch.log
+if not [%ERRORLEVEL%]==[0] @echo **Error! RWRXMLs.xml Generation >> RWRBatch.log
+
+exit /B %ERRORLEVEL%
+
+:end
