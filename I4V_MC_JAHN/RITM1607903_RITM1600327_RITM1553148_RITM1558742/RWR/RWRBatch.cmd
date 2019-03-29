1f2c996d65b5a6d36aa09a5b35363fe327317860
v5x1239 <48722083+v5x1239@users.noreply.github.com>
2019-03-29 08:11:34 -0400

    Add files via upload

---
+++ I4V_MC_JAHN/RITM1607903_RITM1600327_RITM1553148_RITM1558742/RWR/RWRBatch.cmd
@@ -0,0 +1,141 @@
+@echo off
+@if [%1]==[] goto usage
+@if [%2]==[] goto usage
+@if [%3]==[] goto usage
+goto start
+:usage
+@echo RWRBatch.cmd db instance name] [UID] [PID]
+@echo This file is used for InForm RWR changes, to generate MedML based on the SQL statements provided.
+@echo Customers should edit this file with the appropriate SQL file names and submit with the implementation ticket..
+goto end
+
+goto start
+
+:start
+@set dbinstance=%1
+@set uid=%2
+@set pid=%3
+
+cd>%TEMP%\local_dir.tmp
+set OUTBOUND=%TEMP%\local_dir.tmp
+
+@echo RWRBatch.cmd running...
+@echo RWRBatch.cmd running... >> RWRBatch.log
+@echo DB instance %1
+@echo DB instance %1 >> RWRBatch.log
+@echo UID %2
+@echo UID %2 >> RWRBatch.log
+
+
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateFormSection_SAE2001_LF1_igSAE2001_F1_S2_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateFormSection_SAE2001_LF1_igSAE2001_F1_S3_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateFormSection_SAE2001_LF1_igSAE2001_F1_S4_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateFormSection_SAE2001_LF1_igSAE2001_F1_S5_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateFormSection_SAE2001_LF1_igSAE2001_F1_S6_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateFormSection_AE3001_LF1_igAE3001b_F1_S2_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateFormSection_NAEFU2001_LF1_igNAEFU2001_F1_S2_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateFormSection_MH7001_LF1_igMH7001_F1_S2_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateFormSection_MHPRESP1001_LF3_igMHPRESP1001_F3_S1_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateFormSection_CMSI1001_F2_igCMSI1001_F2_S2_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateFormSection_CM1001_LF1_igCM1001_F1_S2_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateFormSection_CM1001_LF2_igCM1001_F2_S2_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateFormSection_STATIXE_F3_igSTATUS_F3_S1_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igEX1001_C2F4_S1_EXSTDAT_EX1001_C2F4_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igEX1001_C2F4_S1_EXENDAT_EX1001_C2F4_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igHRFA1001_F1_S4_HRFARES9_HRFA1001_F1_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igHRFA1001_F1_S1_HRFAASMDAT_HRFA1001_F1_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igHRFA1001_F1_S4_HRFARES7_HRFA1001_F1_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igHRFA1001_F1_S4_HRFARES11_HRFA1001_F1_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igDS1001_F4_S1_DSSTDAT_DS1F4_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igDS1001_F4_S1_DSDECOD_DS1F4_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igAJARR2001_F1_S1_EPAEDAT_AJARRF1_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igAJARR2001_F1_S1_EVDTAGMT_AJARRF1_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igSU1001_F2_S1_SUDAT_SU1001_F2_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igSU1001_F2_S1_SUNCF_ALCOHOL_SU1001_F2_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igHMPR1001_F1_S8_PROCCUR_MRE_HMPR1F1_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igHMPR1001_F1_S3_PROCCUR_US_HMPR1F1_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igHMPR1001_F1_S6_PROCCUR_ERC_HMPR1F1_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igHMPR1001_F1_S7_PROCCUR_MRS_HMPR1F1_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igHMPR1001_F1_S5_PROCCUR_MRC_HMPR1F1_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igHMPR1001_F1_S2_PROCCUR_MRI_HMPR1F1_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igHMPR1001_F1_S4_PROCCUR_CT_HMPR1F1_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igSV1001_S1_VISDAT_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igDS1001_F7_S1_DSSTDAT_DS1F7_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igDS1001_F8_S1_DSSTDAT_DS1F8_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igSU5001_F2_S1_SUDOSADJ_RECDRUG_SU5F2_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igSU1001_F3_S1_SUDOSADJ_ALCOHOL_SU1F3_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igSU1001_F3_S1_SUDOSADJ_ALCOHOL_SU1F3_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igDS1001_F9_S1_DSDECOD_DS1F9_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igDS1001_F9_S1_DSSTDAT_DS1F9_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igDM1001_F3_S1_BRTHYR_DM1001_F3_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igDS1001_F6_S1_DSSTDAT_DS1F6_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igDS2001_F1_S1_DSSTDAT_IC_DS2001_F1_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igLRSS1001_F1_S1_LRSSPERF_LRSS1001_F1_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igBR1001_F1_S1_BRPERF_BR1001_F1_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igEX1001_F4_S1_EXSTDAT_EX1F4_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igDS1001_F5_S1_DSSTDAT_DS1F5_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igSU5001_F1_S1_SUDAT_SU5001_F1_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igEX1001_C1F4_S1_EXSTDAT_EX1C1F4_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igEX1001_F5_S1_EXENDAT_EX1F5_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igCM3001_F1_S1_CMOCCUR_CM3001_F1_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igCM3001_F1_S1_CMONGO_CM3001_F1_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igSAE2001_F1_S1_SAEASMDAT_SAE2F1_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igSAE2001_F1_S1_SAEAWADAT_SAE2F1_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igEPCEV2001_F1_S1_EPAEDAT_EPCEV_F1_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igEPCEV2001_F1_S1_CEVR8_EPCEV_F1_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igAJCEV2001_F1_S1_EPAEDAT_AJCEV_F1_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igAJCEV2001_F1_S1_EVDTAGMT_AJCEV_F1_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igLB9001_F7_S1_LBDAT_LB9F7_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igLB9001_F9_S1_LBDAT_LB9F9_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igEPHSPHF3001_F1_S1_EPAEDAT_EPHSPHF_F1_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igNAEFU2001_F1_S1_NAEFURES3_NAEFU2_F1_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igDA1001_F1_S1_DADAT_DISPAMT_DA1001_F1_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igDA1001_F1_S2_RETAMT_DAPERF_DA1001_F1_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igAJCRV3001_F1_S1_EPAEDAT_AJCRV_F1_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igAJCRV3001_F1_S1_EVDTAGMT_AJCRV_F1_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igSFUQ3001_F1_S1_SFUQEVTFLG_SFUQ3F1_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igAJCGS2001_F1_S1_EPAEDAT_AJCGS_F1_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igAJCGS2001_F1_S1_EVDTAGMT_AJCGS_F1_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igEPCGS2001_F1_S1_EPAEDAT_EPCGS2F1_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igAJVTE1001_F1_EPAEDAT_AJVTE1001_F1_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igAJVTE1001_F1_EVDTAGMT_AJVTE1001_F1_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igAJHSPHF3001_F1_S1_EPAEDAT_AJHSPHFF1_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igAJHSPHF3001_F1_S1_EVDTAGMT_AJHSPHFF1_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igAJDTH2001_F1_S1_EPAEDAT_AJDTHF1_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igAJDTH2001_F1_S1_EVDTAGMT_AJDTHF1_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igINFC2001_PR1001_F1_S1_INFCLOC_INFC2PR1_F1_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igINFC2001_PR1001_F1_S1_INFCSLOCMD_INFC2PR1_F1_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igLB9001_F6_LBDAT_LB9F6_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igAJCIE3001_F1_S1_EPAEDAT_AJCIE_F1_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igAJCIE3001_F1_S1_EVDTAGMT_AJCIE_F1_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igEPRSSD2001_F1_S1_EPAEDAT_EPRSSD2F1_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igEPCRV3001_F1_S1_EPAEDAT_EPCRVF1_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igEPCIE3001_F1_S1_EPAEDAT_EPCIE_F1_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igAJRSSD2001_F1_S1_EPAEDAT_AJRSSDF1_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igAJRSSD2001_F1_S1_EVDTAGMT_AJRSSDF1_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igLB9001_F8_S1_LBDAT_LB9F8_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igEPDTH2001_F1_S1_EPAEDAT_EPDTHF1_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igEPARR2001_F1_S1_EPAEDAT_EPARR2F1_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igEPVTE1001_F1_EPAEDAT_EPVTE1001_F1_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igEC1001_F1_S1_ECSTDAT_EC1F1_1.sql >> RWRBatch.log
+@SQLPLUS  -s -l %uid%/%pid%@%dbinstance% @updateSectionItem_igEC1001_F1_S1_ECENDAT_EC1F1_1.sql >> RWRBatch.log
+
+@echo >> RWRBatch.log
+
+if [%ERRORLEVEL%]==[0] @echo XMLs Generated successfully
+if not [%ERRORLEVEL%]==[0] @echo **Error! XML Generation
+if [%ERRORLEVEL%]==[0] @echo XMLs Generated successfully >> RWRBatch.log
+if not [%ERRORLEVEL%]==[0] @echo **Error! XML Generation >> RWRBatch.log
+
+if not [%ERRORLEVEL%]==[0] goto END
+
+RWRXMLs.vbs %OUTBOUND%
+
+if [%ERRORLEVEL%]==[0] @echo RWRXMLs.xml Generated successfully
+if not [%ERRORLEVEL%]==[0] @echo **Error! RWRXMLs.xml Generation
+if [%ERRORLEVEL%]==[0] @echo RWRXMLs.xml Generated successfully >> RWRBatch.log
+if not [%ERRORLEVEL%]==[0] @echo **Error! RWRXMLs.xml Generation >> RWRBatch.log
+
+exit /B %ERRORLEVEL%
+
+:end
